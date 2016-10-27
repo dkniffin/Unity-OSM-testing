8e1b50d5d233db3846360bbc97c17217db86385f
Derek Kniffin <derek.kniffin@gmail.com>
2016-10-27 19:41:32 -0400

    Move some methods into OSMData

--- Assets/Scripts/OSM/OSMData.cs
+++ Assets/Scripts/OSM/OSMData.cs
@@ -1,15 +1,55 @@
 ﻿using System;
+using System.Xml;
+using System.IO;
+using System.Text;
 using System.Collections.Generic;
+using OSM;
 
 namespace OSM {
 	public class OSMData {
+		private string _xmlContent;
+		private XmlReader _reader;
+		private Element _currentElement;
+
 		private Dictionary<long, Node> nodes = new Dictionary<long, Node> ();
 		private Dictionary<long, Way> ways = new Dictionary<long, Way>();
 		private LatLonBounds bounds = new LatLonBounds ();
 
-		public void AddNode(Node node) {
-			nodes[node.id] = node;
+		public void Import(string osmfilename) {
+			_xmlContent = File.ReadAllText (osmfilename);
+			_reader = XmlReader.Create (new StringReader (_xmlContent));
+
+			while (_reader.Read ()) {
+				if (_reader.NodeType == XmlNodeType.Element) {
+					switch (_reader.Name) {
+					case "node":
+						parseNode ();
+						break;
+					case "tag":
+						ParseTag ();
+						break;
+					case "nd":
+						ParseNd ();
+						break;
+					case "way":
+						ParseWay ();
+						break;
+					/*
+					case "relation":
+						ParseRelation ();
+						break;
+					case "member":
+						ParseMember ();
+						break;
+			    	*/
+					case "bounds":
+						ParseBounds ();
+						break;
+					}
+				}
+			}
 		}
+
 
 		public Dictionary<long, Node> GetNodes() {
 			return nodes;
@@ -17,10 +57,6 @@ namespace OSM {
 
 		public Node GetNodeById(long id) {
 			return nodes [id];
-		}
-
-		public void AddWay(Way way) {
-			ways[way.id] = way;
 		}
 
 		public Dictionary<long, Way> GetWays() {
@@ -31,15 +67,61 @@ namespace OSM {
 			return ways [id];
 		}
 
-		public void SetBounds(double n, double e, double s, double w) {
+		public LatLonBounds GetBounds() {
+			return bounds;
+		}
+
+		private void parseNode() {
+			Node node = new Node ();
+			node.id = long.Parse(_reader.GetAttribute("id"));
+			node.latitude = float.Parse(_reader.GetAttribute("lat"));
+			node.longitude = float.Parse(_reader.GetAttribute("lon"));
+			_currentElement = node;
+			AddNode(node);
+		}
+
+		private void ParseWay() {
+			Way way = new Way ();
+			way.id = long.Parse(_reader.GetAttribute("id"));
+			_currentElement = way;
+			AddWay(way);
+		}
+
+		private void ParseNd() {
+			long node_id = long.Parse(_reader.GetAttribute("ref"));
+			Node node = GetNodeById(node_id);
+			(_currentElement as Way).AddNode(node);
+		}
+
+		private void ParseTag() {
+			var key = _reader.GetAttribute("k");
+			var value = _reader.GetAttribute("v");
+			_currentElement.tags[key] = value;
+		}
+
+		private void ParseBounds() {
+			var n = double.Parse(_reader.GetAttribute("maxlat"));
+			var e = double.Parse(_reader.GetAttribute("maxlon"));
+			var s = double.Parse(_reader.GetAttribute("minlat"));
+			var w = double.Parse(_reader.GetAttribute("minlon"));
+
+			SetBounds (n, e, s, w);
+		}
+
+
+		private void AddNode(Node node) {
+			nodes[node.id] = node;
+		}
+
+		private void AddWay(Way way) {
+			ways[way.id] = way;
+		}
+
+		private void SetBounds(double n, double e, double s, double w) {
 			bounds.n = n;
 			bounds.e = e;
 			bounds.s = s;
 			bounds.w = w;
-		}
-
-		public LatLonBounds GetBounds() {
-			return bounds;
 		}
 	}
 }
--- Assets/Scripts/OSMImporter.cs
+++ Assets/Scripts/OSMImporter.cs
@@ -1,8 +1,5 @@
 ﻿using UnityEngine;
 using System;
-using System.Xml;
-using System.IO;
-using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -13,13 +10,10 @@ public class OSMImporter : MonoBehaviour {
 	OSMData osmData = new OSMData();
 
 	public GameObject buildingPrefab;
-	private string _xmlContent;
-	private XmlReader _reader;
-	private Element _currentElement;
 
 	public void Import() {
 		// Import OSM Data
-		ImportOSMData ();
+		osmData.Import ("Assets/atc.osm");
 
 		// Delete existing children
 		foreach (Transform child in transform) {
@@ -29,80 +23,7 @@ public class OSMImporter : MonoBehaviour {
 		// Draw the scene
 		DrawBuildings ();
 	}
-
-	private void ImportOSMData() {
-		_xmlContent = File.ReadAllText ("Assets/atc.osm");
-		_reader = XmlReader.Create (new StringReader (_xmlContent));
-
-		while (_reader.Read ()) {
-			if (_reader.NodeType == XmlNodeType.Element) {
-				switch (_reader.Name) {
-				case "node":
-					parseNode ();
-					break;
-				case "tag":
-					ParseTag ();
-					break;
-				case "nd":
-					ParseNd ();
-					break;
-				case "way":
-					ParseWay ();
-					break;
-			    /*
-				case "relation":
-					ParseRelation ();
-					break;
-				case "member":
-					ParseMember ();
-					break;
-			    */
-				case "bounds":
-					ParseBounds ();
-					break;
-				}
-			}
-		}
-
-	}
-
-	private void parseNode() {
-		Node node = new Node ();
-		node.id = long.Parse(_reader.GetAttribute("id"));
-		node.latitude = float.Parse(_reader.GetAttribute("lat"));
-		node.longitude = float.Parse(_reader.GetAttribute("lon"));
-		_currentElement = node;
-		osmData.AddNode(node);
-	}
-
-	private void ParseWay() {
-		Way way = new Way ();
-		way.id = long.Parse(_reader.GetAttribute("id"));
-		_currentElement = way;
-		osmData.AddWay(way);
-	}
-
-	private void ParseNd() {
-		long node_id = long.Parse(_reader.GetAttribute("ref"));
-		Node node = osmData.GetNodeById(node_id);
-		(_currentElement as Way).AddNode(node);
-	}
-
-	private void ParseTag() {
-		var key = _reader.GetAttribute("k");
-		var value = _reader.GetAttribute("v");
-		_currentElement.tags[key] = value;
-	}
-
-	private void ParseBounds() {
-		var n = double.Parse(_reader.GetAttribute("maxlat"));
-		var e = double.Parse(_reader.GetAttribute("maxlon"));
-		var s = double.Parse(_reader.GetAttribute("minlat"));
-		var w = double.Parse(_reader.GetAttribute("minlon"));
-
-		osmData.SetBounds (n, e, s, w);
-	}
-
+		
 	private void DrawBuildings() {
 		foreach (Way way in osmData.GetWays().Values) {
 			if (!way.HasTag("building")) {
